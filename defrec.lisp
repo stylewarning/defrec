042747205447494bca9040aa2726e6e7b3450f41
Robert Smith <quad@symbo1ics.com>
2013-02-26 21:55:19 -0800

    fix DEFREC to actually expand to the code we want.

--- defrec.lisp
+++ defrec.lisp
@@ -3,6 +3,8 @@
 
 (in-package #:defrec)
 
+;;;; XXX: This code is really ugly. Could use a facelift sometime.
+
 (defmacro defrec (&body definitions)
   "Define mutually recursive functions. Each definition of DEFINITIONS
   should be of the same form of FLET or LABELS:
@@ -10,24 +12,35 @@
   (<function name> <lambda list>
     <function body>)."
   (let* ((f-names (mapcar #'first definitions))
-         (gensyms (mapcar (lambda (f-name)
+         (decls   (mapcar (lambda (defn)
+                            (nth-value
+                             1
+                             (alexandria:parse-body (cddr defn))))
+                          definitions))
+         (g-names (mapcar (lambda (f-name)
                             (gensym (symbol-name f-name)))
                           f-names))
          (g-defns (mapcar (lambda (g defn)
                             (cons g (cdr defn)))
-                          gensyms
+                          g-names
                           definitions)))
     `(labels (,@definitions)
-       (flet ,g-defns
-         (declare (inline ,@gensyms))
-         ,@(mapcar (lambda (f-name g-defn)
+       (flet ,(mapcar (lambda (g-name f-name decl lam-list)
+                        `(,g-name ,lam-list
+                            ,@decl
+                            (,f-name ,@lam-list)))
+                      g-names
+                      f-names
+                      decls
+                      (mapcar #'second definitions))
+         
+         (declare (inline ,@g-names))
+         ,@(mapcar (lambda (f-name decl g-defn)
                      `(cl:defun ,f-name ,(second g-defn)
-                        ;; Declarations
-                        ,@(nth-value
-                           1
-                           (alexandria:parse-body (cddr g-defn)))
-                        ;; Call to FLET function
+                        ,@decl
                         (,(first g-defn) ,@(second g-defn))))
                    f-names
-                   g-defns)))))
+                   decls
+                   g-defns))
+       ',f-names)))
 
