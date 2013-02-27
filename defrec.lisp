cb45423c91495c908ce53c26379d60a837f41867
Robert Smith <quad@symbo1ics.com>
2013-02-26 22:14:09 -0800

    Handle nullary expansion, specialize unary expansion. Add a note about where it fails.

--- defrec.lisp
+++ defrec.lisp
@@ -5,6 +5,8 @@
 
 ;;;; XXX: This code is really ugly. Could use a facelift sometime.
 
+;;;; FIXME: Doesn't handle &foo arguments.
+
 (defmacro defrec (&body definitions)
   "Define mutually recursive functions. Each definition of DEFINITIONS
   should be of the same form of FLET or LABELS:
@@ -20,36 +22,41 @@
           ()
           "Every DEFREC definition must have the form ~
            (symbol lambda-list body...).")
-  (let* ((f-names (mapcar #'first definitions))
-         (decls   (mapcar (lambda (defn)
-                            (nth-value
-                             1
-                             (alexandria:parse-body (cddr defn))))
-                          definitions))
-         (g-names (mapcar (lambda (f-name)
-                            (gensym (symbol-name f-name)))
-                          f-names))
-         (g-defns (mapcar (lambda (g defn)
-                            (cons g (cdr defn)))
-                          g-names
-                          definitions)))
-    `(labels (,@definitions)
-       (flet ,(mapcar (lambda (g-name f-name decl lam-list)
-                        `(,g-name ,lam-list
-                            ,@decl
-                            (,f-name ,@lam-list)))
-                      g-names
+  (when (null definitions)
+    (return-from defrec nil))
+  (cond
+    ((null definitions) nil)
+    ((null (cdr definitions)) (cons 'cl:defun (first definitions)))
+    (t
+     (let* ((f-names (mapcar #'first definitions))
+            (decls   (mapcar (lambda (defn)
+                               (nth-value
+                                1
+                                (alexandria:parse-body (cddr defn))))
+                             definitions))
+            (g-names (mapcar (lambda (f-name)
+                               (gensym (symbol-name f-name)))
+                             f-names))
+            (g-defns (mapcar (lambda (g defn)
+                               (cons g (cdr defn)))
+                             g-names
+                             definitions)))
+       `(labels (,@definitions)
+          (flet ,(mapcar (lambda (g-name f-name decl lam-list)
+                           `(,g-name ,lam-list
+                                     ,@decl
+                                     (,f-name ,@lam-list)))
+                  g-names
+                  f-names
+                  decls
+                  (mapcar #'second definitions))
+            
+            (declare (inline ,@g-names))
+            ,@(mapcar (lambda (f-name decl g-defn)
+                        `(cl:defun ,f-name ,(second g-defn)
+                           ,@decl
+                           (,(first g-defn) ,@(second g-defn))))
                       f-names
                       decls
-                      (mapcar #'second definitions))
-         
-         (declare (inline ,@g-names))
-         ,@(mapcar (lambda (f-name decl g-defn)
-                     `(cl:defun ,f-name ,(second g-defn)
-                        ,@decl
-                        (,(first g-defn) ,@(second g-defn))))
-                   f-names
-                   decls
-                   g-defns))
-       ',f-names)))
-
+                      g-defns))
+          ',f-names)))))
